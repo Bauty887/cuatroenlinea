f142ae5ff0a831c96e370280c8749d473ca760e0
Bauty887 <bautycoc@gmail.com>
2022-09-13 16:31:37 -0300

    Probando si funciona el TA

--- tests/Feature/TableroTest.php
+++ tests/Feature/TableroTest.php
@@ -6,37 +6,38 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class TableroTest extends TestCase{
+/** @test */
 public function TestearTablero(){
 
 // Creo un tablero de (x,y) dimensiones y dos variables con los colores de la ficha
 $t = new Tablero(5,5);
-$FichaR = new Ficha('roja');
+$FichaR = new Ficha('rojo');
 $FichaA = new Ficha('azul');
 
 // Suelto fichas en posiciones al azar
-$t->SoltarFicha(3,$FichaR());
-$t->SoltarFicha(3,$FichaA());
-$t->SoltarFicha(3,$FichaR());
-$t->SoltarFicha(2,$FichaA());
-$t->SoltarFicha(1,$FichaR());
-$t->SoltarFicha(4,$FichaA());
+$t->SoltarFicha(3,$FichaR);
+$t->SoltarFicha(3,$FichaA);
+$t->SoltarFicha(3,$FichaR);
+$t->SoltarFicha(2,$FichaA);
+$t->SoltarFicha(1,$FichaR);
+$t->SoltarFicha(4,$FichaA);
 
 $rojas = 0;
 $azules = 0;
 $blancas = 0;
 
 // Cuento fichas rojas y azules en el tablero
-for ($x=0; $x<$this->Altura; $x++) {
-    for ($y=0; $y<$this->Ancho; $y++) {
+for ($x=0; $x<$t->Altura; $x++) {
+    for ($y=0; $y<$t->Ancho; $y++) {
     if(($t->tablero[$x][$y]->ConseguirColor()) == 'azul'){
     $azules++;
-    }else if(($t->tablero[$x][$y]->ConseguirColor()) == 'roja'){
+    }else if(($t->tablero[$x][$y]->ConseguirColor()) == 'rojo'){
             $rojas++;
     }else $blancas++;
     }
 }
 
-$this->assertTrue($blancas == 5*5-($rojas+$azules));
+$this->assertTrue($blancas == (25-($rojas+$azules)));
 
 }
 }
